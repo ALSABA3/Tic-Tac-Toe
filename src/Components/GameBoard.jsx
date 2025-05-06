c35c2fae795c3f1a45271b31610e09f4b3188e47
ALSABA3 <202003001107@dogus.edu.tr>
2025-05-06 12:16:06 +0300

    first commit

---
+++ src/Components/GameBoard.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const GameBoard = ({ onSelectSquare, board }) => {
+  //   const [gameBoard, setGameBoard] = useState(intialGameBoard);
+
+  //   const handleSelectSquare = (rowIndex, colIndex) => {
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedGameBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       return updatedGameBoard;
+  //     });
+  //     onSelectSquare();
+  //   };
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
